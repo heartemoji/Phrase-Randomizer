67568a739161ef12c4bde0dc1d0c73739d8441ac
heartemoji <thomasamaral2016@gmail.com>
2018-01-31 11:52:21 -0500

    Added foundation for reading from file functionality

--- randomizer.cpp
+++ randomizer.cpp
@@ -10,13 +10,43 @@
 using namespace std;
 
 
+void directInput();
+void fromFile();
+
 
 int main(){
 
+    cout << "Phrase Randomizer" << endl;
+    cout << "Make a selection: " << endl;
+    cout << "1. Direct Console Input" << endl;
+    cout << "2. Read from a file" << endl;
+    cout << ">>:";
+    
+    char selection;
+    cin >> selection;
+
+    switch(selection){
+        case 1:
+            directInput();
+            break;
+
+        case 2:
+            break;
+
+        default:
+            cout << "Please enter a valid selection";
+            break;
+    }
+    
+
+    return 0;
+}
+
+void directInput(){
     vector<string> lines;
 
     string line;
-    
+
     cout << "Enter some lines! (Ctrl-D to quit)" << '\n';
 
     while(getline(cin, line)){
@@ -30,6 +60,6 @@ int main(){
     for(string l : lines){
         cout << l << '\n';
     }
-
-    return 0;
 }
+
+void fromFile(){}
