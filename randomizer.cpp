1e1ad1f18922d045dd24f30668fc2e2818bced7a
heartemoji <thomasamaral2016@gmail.com>
2018-02-02 12:51:20 -0500

    Added reading from file functionality. Moved shuffling logic to main method to avoid repeated code. Input generation methods now return the generated list.

--- randomizer.cpp
+++ randomizer.cpp
@@ -6,12 +6,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
 
-void directInput();
-void fromFile();
+vector<string> directInput();
+vector<string> fromFile();
 
 
 int main(){
@@ -25,41 +27,77 @@ int main(){
     int selection;
     cin >> selection;
 
+    vector<string> lines;
+
     switch(selection){
         case 1:
-            directInput();
+            lines = directInput();
             break;
 
         case 2:
+            lines = fromFile();
             break;
 
         default:
             cout << "Please enter a valid selection";
-            break;
-    }
+            return 0;
+        }
+        
+    cout << endl;
     
+    if(lines.empty())
+        return 1;
+
+    random_shuffle(lines.begin() + 1, lines.end());
+
+    for(string l : lines){
+        cout << l << endl;
+    }
+
+    
+        
+
 
     return 0;
 }
 
-void directInput(){
+vector<string> directInput(){
     vector<string> lines;
 
     string line;
 
-    cout << "Enter some lines! (Ctrl-D to quit)" << '\n';
+    cout << "Enter some lines! (Ctrl-D to quit)" << endl;
 
     while(getline(cin, line)){
         lines.push_back(line);
     }
     
-    cout << endl;
-
-    random_shuffle(lines.begin() + 1, lines.end());
-
-    for(string l : lines){
-        cout << l << '\n';
-    }
+    return lines;
+    
 }
 
-void fromFile(){}
+vector<string> fromFile(){
+
+    cout << "Enter path to file: ";
+    string filePath;
+    cin >> filePath;
+
+    vector<string> lines;
+    string line;
+    
+    fstream infile(filePath, fstream::in);
+
+    if(infile.fail()){
+        cout << "File not found: " << filePath;
+        return lines;
+    }
+
+    while(getline(infile, line)){
+        lines.push_back(line);
+    }
+    
+    infile.close();
+
+    return lines;
+    
+}
